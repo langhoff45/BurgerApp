0d2d0e0ac24e8e7f9cc317569d2e3e283691b6d9
langhoff45 <30696376+langhoff45@users.noreply.github.com>
2019-01-09 14:37:14 +0100

    Styling til toolbar initiliseret og grund kode til toolbar komponent.

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const toolbar = () => (
+    <header>
+        <div>Menu</div>
+        <div>Logo</div>
+        <nav>
+            <ul>
+                ...
+            </ul>
+        </nav>
+    </header>
+);
+
+export default toolbar;

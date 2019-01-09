aa414e77fed54a91fec072df286f2829762eda11
langhoff45 <30696376+langhoff45@users.noreply.github.com>
2019-01-09 15:39:49 +0100

    Mobil venlig sidebar første lektion.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Classes from './Layout.css'
 
 import Aux from '../../hoc/Aux2';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
 const Layout = (props) => (
     <Aux>
         <Toolbar />
+        <Sidedrawer />
         <main className={Classes.Content}>
             {props.children}
         </main>

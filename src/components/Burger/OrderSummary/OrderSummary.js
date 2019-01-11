5324abfbc69ae0be093bed389cfdc24518dfd830
langhoff45 <30696376+langhoff45@users.noreply.github.com>
2019-01-11 13:14:21 +0100

    Performance forbedring i Modal, så antallet af ingredienser kun opateres når modal vinduet vises.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Aux2';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+    //this could be a functional component, doesent have to be a class
+    componentWillUpdate() {
+        console.log("[OrderSummary] willUpdate")
+    }
 
-    const ingredientSummary = Object.keys(props.ingredients)
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
             .map(igKey =>  {
                 return  <li key={igKey}>
                             <span style={{textTransform: 'capitalize'}}>{igKey}</span>:
-                            {props.ingredients[igKey]}
+                            {this.props.ingredients[igKey]}
                         </li>
             });
 
-    return (
-        <Aux>
+        return ( <Aux>
             <h3>Your order</h3>
             <p>A pusheen burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>Cancel</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
         </Aux>
-    );
-}
+        );
+    }
+} 
 
-export default orderSummary;
+export default OrderSummary;

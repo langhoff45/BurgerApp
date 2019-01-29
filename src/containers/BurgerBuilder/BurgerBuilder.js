716be480a5e17ac7963102453ccd170c27e28c59
langhoff45 <30696376+langhoff45@users.noreply.github.com>
2019-01-29 15:23:02 +0100

    Send data to database. Axios file created for orders.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -85,7 +87,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("Continue");
+       // alert("Continue");
+       const order = {
+           ingredients: this.state.ingredients,
+           price: this.state.totalPrice,
+           customer: {
+               name: "Pusheen",
+               address: {
+                    street: "teststreet 1",
+                    zipcode: "2200",
+                    country: "Denmark"
+               },
+               email: 'test@test.dk'
+           },
+           deliveryMethod: 'fastest'
+       }
+
+       axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
